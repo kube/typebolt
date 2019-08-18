b95b3ece1aff94b4e0ebd68b4c6e363e110e0718
Chris Feijoo <chris@feijoo.fr>
2019-08-18 16:25:14 +0200

    Capitalize Assert True/False to prevent issues in type definitions

--- lib/__tests__/Logic.spec.ts
+++ lib/__tests__/Logic.spec.ts
@@ -14,57 +14,57 @@ import { And, Or, Not, Xor, Nor, Nand, Xnor } from '../Logic'
 //
 // Not
 //
-Assert.false<Not<true>>()
-Assert.true<Not<false>>()
+Assert.False<Not<true>>()
+Assert.True<Not<false>>()
 
 //
 // And
 //
-Assert.true<And<true, true>>()
-Assert.false<And<true, false>>()
-Assert.false<And<false, true>>()
-Assert.false<And<false, false>>()
+Assert.True<And<true, true>>()
+Assert.False<And<true, false>>()
+Assert.False<And<false, true>>()
+Assert.False<And<false, false>>()
 Assert<IsExactType<boolean, And<boolean, true>>>()
 Assert<IsExactType<boolean, And<boolean, boolean>>>()
 Assert<IsExactType<boolean, And<true, boolean>>>()
-Assert.false<And<false, boolean>>()
+Assert.False<And<false, boolean>>()
 
 //
 // Or
 //
-Assert.true<Or<true, true>>()
-Assert.true<Or<true, false>>()
-Assert.true<Or<false, true>>()
-Assert.false<Or<false, false>>()
+Assert.True<Or<true, true>>()
+Assert.True<Or<true, false>>()
+Assert.True<Or<false, true>>()
+Assert.False<Or<false, false>>()
 
 //
 // Xor
 //
-Assert.false<Xor<true, true>>()
-Assert.true<Xor<true, false>>()
-Assert.true<Xor<false, true>>()
-Assert.false<Xor<false, false>>()
+Assert.False<Xor<true, true>>()
+Assert.True<Xor<true, false>>()
+Assert.True<Xor<false, true>>()
+Assert.False<Xor<false, false>>()
 
 //
 // Nor
 //
-Assert.false<Nor<true, true>>()
-Assert.false<Nor<true, false>>()
-Assert.false<Nor<false, true>>()
-Assert.true<Nor<false, false>>()
+Assert.False<Nor<true, true>>()
+Assert.False<Nor<true, false>>()
+Assert.False<Nor<false, true>>()
+Assert.True<Nor<false, false>>()
 
 //
 // Nand
 //
-Assert.false<Nand<true, true>>()
-Assert.true<Nand<true, false>>()
-Assert.true<Nand<false, true>>()
-Assert.true<Nand<false, false>>()
+Assert.False<Nand<true, true>>()
+Assert.True<Nand<true, false>>()
+Assert.True<Nand<false, true>>()
+Assert.True<Nand<false, false>>()
 
 //
 // Xnor
 //
-Assert.true<Xnor<true, true>>()
-Assert.false<Xnor<true, false>>()
-Assert.false<Xnor<false, true>>()
-Assert.true<Xnor<false, false>>()
+Assert.True<Xnor<true, true>>()
+Assert.False<Xnor<true, false>>()
+Assert.False<Xnor<false, true>>()
+Assert.True<Xnor<false, false>>()
--- lib/__tests__/Testing.spec.ts
+++ lib/__tests__/Testing.spec.ts
@@ -22,33 +22,33 @@ import {
 //
 {
   Assert<true>()
-  Assert.true<true>()
-  Assert.false<false>()
+  Assert.True<true>()
+  Assert.False<false>()
 }
 
 //
 // IsType
 //
 {
-  Assert.true<IsType<boolean, true>>()
-  Assert.true<IsType<boolean, false>>()
-  Assert.true<IsType<string, "Hello">>()
-  Assert.true<IsType<number, 42>>()
-  Assert.true<IsType<number, 42>>()
+  Assert.True<IsType<boolean, true>>()
+  Assert.True<IsType<boolean, false>>()
+  Assert.True<IsType<string, "Hello">>()
+  Assert.True<IsType<number, 42>>()
+  Assert.True<IsType<number, 42>>()
 }
 
 //
 // IsMutualSubType
 //
 {
-  Assert.true<IsMutualSubType<any, any>>()
-  Assert.true<IsMutualSubType<null, null>>()
-  Assert.true<IsMutualSubType<undefined, undefined>>()
-  Assert.true<IsMutualSubType<string, string>>()
-  Assert.true<IsMutualSubType<number, number>>()
-  Assert.true<IsMutualSubType<{}, {}>>()
-  Assert.true<IsMutualSubType<[], []>>()
-  Assert.true<IsMutualSubType<number, number>>()
+  Assert.True<IsMutualSubType<any, any>>()
+  Assert.True<IsMutualSubType<null, null>>()
+  Assert.True<IsMutualSubType<undefined, undefined>>()
+  Assert.True<IsMutualSubType<string, string>>()
+  Assert.True<IsMutualSubType<number, number>>()
+  Assert.True<IsMutualSubType<{}, {}>>()
+  Assert.True<IsMutualSubType<[], []>>()
+  Assert.True<IsMutualSubType<number, number>>()
 
   Assert<IsType<boolean, IsMutualSubType<true | false, true | false>>>()
 }
@@ -57,79 +57,79 @@ import {
 // IsUnion
 //
 {
-  Assert.true<IsUnion<'Hello' | 'World'>>()
-  Assert.true<IsUnion<1 | 2>>()
-  Assert.true<IsUnion<boolean>>() // Boolean is union of true and false
-  Assert.true<IsUnion<true | false>>()
-  Assert.true<IsUnion<object | undefined>>()
+  Assert.True<IsUnion<'Hello' | 'World'>>()
+  Assert.True<IsUnion<1 | 2>>()
+  Assert.True<IsUnion<boolean>>() // Boolean is union of true and false
+  Assert.True<IsUnion<true | false>>()
+  Assert.True<IsUnion<object | undefined>>()
 
-  Assert.false<IsUnion<any>>()
-  Assert.false<IsUnion<any | 42>>() // Any cannot be unioned
-  Assert.false<IsUnion<string>>()
-  Assert.false<IsUnion<string | string>>()
-  Assert.false<IsUnion<string | "Hello">>()
-  Assert.false<IsUnion<number>>()
-  Assert.false<IsUnion<number | number>>()
-  Assert.false<IsUnion<number | 42>>()
-  Assert.false<IsUnion<any | any>>()
-  Assert.false<IsUnion<null | null>>()
-  Assert.false<IsUnion<object | object>>()
-  Assert.false<IsUnion<undefined | undefined>>()
+  Assert.False<IsUnion<any>>()
+  Assert.False<IsUnion<any | 42>>() // Any cannot be unioned
+  Assert.False<IsUnion<string>>()
+  Assert.False<IsUnion<string | string>>()
+  Assert.False<IsUnion<string | "Hello">>()
+  Assert.False<IsUnion<number>>()
+  Assert.False<IsUnion<number | number>>()
+  Assert.False<IsUnion<number | 42>>()
+  Assert.False<IsUnion<any | any>>()
+  Assert.False<IsUnion<null | null>>()
+  Assert.False<IsUnion<object | object>>()
+  Assert.False<IsUnion<undefined | undefined>>()
 }
 
 //
 // IsAny
 //
 {
-  Assert.true<IsAny<any>>()
+  Assert.True<IsAny<any>>()
 
-  Assert.false<IsAny<null>>()
-  Assert.false<IsAny<number>>()
-  Assert.false<IsAny<string>>()
-  Assert.false<IsAny<boolean>>()
-  Assert.false<IsAny<number | string>>()
-  Assert.false<IsAny<42>>()
-  Assert.false<IsAny<true>>()
+  Assert.False<IsAny<null>>()
+  Assert.False<IsAny<number>>()
+  Assert.False<IsAny<string>>()
+  Assert.False<IsAny<boolean>>()
+  Assert.False<IsAny<number | string>>()
+  Assert.False<IsAny<42>>()
+  Assert.False<IsAny<true>>()
 }
 
 //
 // IsExactType
 //
 {
-  Assert.true<IsExactType<any, any>>()
-  Assert.true<IsExactType<null, null>>()
-  Assert.true<IsExactType<undefined, undefined>>()
-  Assert.true<IsExactType<string, string>>()
-  Assert.true<IsExactType<number, number>>()
-  Assert.true<IsExactType<boolean, boolean>>()
-  Assert.true<IsExactType<false, false>>()
-  Assert.true<IsExactType<true, true>>()
-  Assert.true<IsExactType<42, 42>>()
-  Assert.true<IsExactType<"Hello", "Hello">>()
-  Assert.true<IsExactType<string | number, string | number>>()
-  Assert.true<IsExactType<"Hello" | "World", "Hello" | "World">>()
-  Assert.true<IsExactType<"Hello" | 42, "Hello" | 42>>()
-  Assert.true<IsExactType<true | false, true | false>>()
-  Assert.true<IsExactType<{}, {}>>()
-  Assert.true<IsExactType<
+  Assert.True<IsExactType<any, any>>()
+  Assert.True<IsExactType<null, null>>()
+  Assert.True<IsExactType<undefined, undefined>>()
+  Assert.True<IsExactType<string, string>>()
+  Assert.True<IsExactType<number, number>>()
+  Assert.True<IsExactType<boolean, boolean>>()
+  Assert.True<IsExactType<false, false>>()
+  Assert.True<IsExactType<true, true>>()
+  Assert.True<IsExactType<42, 42>>()
+  Assert.True<IsExactType<"Hello", "Hello">>()
+  Assert.True<IsExactType<string | number, string | number>>()
+  Assert.True<IsExactType<"Hello" | "World", "Hello" | "World">>()
+  Assert.True<IsExactType<"Hello" | 42, "Hello" | 42>>()
+  Assert.True<IsExactType<true | false, true | false>>()
+  Assert.True<IsExactType<{}, {}>>()
+  Assert.True<IsExactType<
     { name: string, age: number },
     { name: string, age: number }>>()
 
-  Assert.false<IsExactType<true, false>>()
-  Assert.false<IsExactType<false, true>>()
-  Assert.false<IsExactType<boolean, false>>()
-  Assert.false<IsExactType<boolean, true>>()
-  Assert.false<IsExactType<true, boolean>>()
-  Assert.false<IsExactType<false, boolean>>()
-  Assert.false<IsExactType<false, boolean>>()
-  Assert.false<IsExactType<null, any>>()
-  Assert.false<IsExactType<any, null>>()
-  Assert.false<IsExactType<any, 42>>()
-  Assert.false<IsExactType<any, 42 | string>>()
-  Assert.false<IsExactType<
+  Assert.False<IsExactType<true, false>>()
+  Assert.False<IsExactType<false, true>>()
+  Assert.False<IsExactType<boolean, false>>()
+  Assert.False<IsExactType<boolean, true>>()
+  Assert.False<IsExactType<true, boolean>>()
+  Assert.False<IsExactType<false, boolean>>()
+  Assert.False<IsExactType<false, boolean>>()
+  Assert.False<IsExactType<null, any>>()
+  Assert.False<IsExactType<any, null>>()
+  Assert.False<IsExactType<any, 42>>()
+  Assert.False<IsExactType<any, 42 | string>>()
+  Assert.False<IsExactType<
     { name: string },
     { name: string, age: number }>>()
-  Assert.false<IsExactType<
+  Assert.False<IsExactType<
     { name: string, age: number },
     { name: string }>>()
 }
