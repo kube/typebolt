d2edb21032da441bf7adc401e68a46ebdcd1ae78
Chris Feijoo <chris@kube.io>
2020-09-01 00:42:13 +0200

    Include boolean in Logic Testing

--- lib/__tests__/Logic.spec.ts
+++ lib/__tests__/Logic.spec.ts
@@ -16,6 +16,7 @@ import { And, Or, Not, Xor, Nor, Nand, Xnor } from "../Logic";
 //
 Assert.False<Not<true>>();
 Assert.True<Not<false>>();
+Assert<IsExactType<boolean, Not<boolean>>>();
 
 //
 // And
@@ -27,7 +28,7 @@ Assert.False<And<false, false>>();
 Assert<IsExactType<boolean, And<boolean, true>>>();
 Assert<IsExactType<boolean, And<boolean, boolean>>>();
 Assert<IsExactType<boolean, And<true, boolean>>>();
-Assert.False<And<false, boolean>>();
+Assert<IsExactType<false, And<false, boolean>>>();
 
 //
 // Or
@@ -36,6 +37,10 @@ Assert.True<Or<true, true>>();
 Assert.True<Or<true, false>>();
 Assert.True<Or<false, true>>();
 Assert.False<Or<false, false>>();
+Assert<IsExactType<true, Or<boolean, true>>>();
+Assert<IsExactType<true, Or<true, boolean>>>();
+Assert<IsExactType<boolean, Or<boolean, false>>>();
+Assert<IsExactType<boolean, Or<false, boolean>>>();
 
 //
 // Xor
@@ -44,6 +49,10 @@ Assert.False<Xor<true, true>>();
 Assert.True<Xor<true, false>>();
 Assert.True<Xor<false, true>>();
 Assert.False<Xor<false, false>>();
+Assert<IsExactType<boolean, Xor<boolean, true>>>();
+Assert<IsExactType<boolean, Xor<true, boolean>>>();
+Assert<IsExactType<boolean, Xor<boolean, false>>>();
+Assert<IsExactType<boolean, Xor<false, boolean>>>();
 
 //
 // Nor
@@ -52,6 +61,10 @@ Assert.False<Nor<true, true>>();
 Assert.False<Nor<true, false>>();
 Assert.False<Nor<false, true>>();
 Assert.True<Nor<false, false>>();
+Assert<IsExactType<false, Nor<boolean, true>>>();
+Assert<IsExactType<false, Nor<true, boolean>>>();
+Assert<IsExactType<boolean, Nor<boolean, false>>>();
+Assert<IsExactType<boolean, Nor<false, boolean>>>();
 
 //
 // Nand
@@ -60,6 +73,10 @@ Assert.False<Nand<true, true>>();
 Assert.True<Nand<true, false>>();
 Assert.True<Nand<false, true>>();
 Assert.True<Nand<false, false>>();
+Assert<IsExactType<boolean, Nand<boolean, true>>>();
+Assert<IsExactType<boolean, Nand<boolean, boolean>>>();
+Assert<IsExactType<boolean, Nand<true, boolean>>>();
+Assert<IsExactType<true, Nand<false, boolean>>>();
 
 //
 // Xnor
@@ -68,3 +85,7 @@ Assert.True<Xnor<true, true>>();
 Assert.False<Xnor<true, false>>();
 Assert.False<Xnor<false, true>>();
 Assert.True<Xnor<false, false>>();
+Assert<IsExactType<boolean, Xnor<boolean, true>>>();
+Assert<IsExactType<boolean, Xnor<true, boolean>>>();
+Assert<IsExactType<boolean, Xnor<boolean, false>>>();
+Assert<IsExactType<boolean, Xnor<false, boolean>>>();
